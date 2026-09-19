de53666ce75d7fef0f0bac43ea612c80019f06d7
Hiago 7k <hiago7k@gmail.com>
2026-09-19 14:48:42 -0300

    dsa

--- src/DividirParaConquistar.java
+++ src/DividirParaConquistar.java
@@ -3,7 +3,7 @@ public class DividirParaConquistar {
         // RecursividadeFatorial chama = new RecursividadeFatorial();
 
         MaiorNumeroDeUmArray maior = new MaiorNumeroDeUmArray();
-        System.out.println(maior.maiorNumero(new int[]{1, 3, 5, 6}, 0));
+        System.out.println(maior.maiorNumero(new int[]{2, 3, 5, 1}, 0));
     }
 
 }
--- src/MaiorNumeroDeUmArray.java
+++ src/MaiorNumeroDeUmArray.java
@@ -2,19 +2,19 @@ import java.util.Arrays;
 
 public class MaiorNumeroDeUmArray {
         int maiorNumero (int[] nums, int i){
-
-            if(nums[i] < nums[i +1]){
-               nums[i] = nums[i +1];
-
-            }else {
-
-                if (i <= nums.length -1){
-                return  nums[i];
+            i = 0;
+            int j = 0;
+            int higher = 0;
+            if(j <= nums.length -1){
+                j++;
+                if (nums[i] <= nums[i+1]){
+                    higher = nums[i]; // caso base
+                }else {
+                    i++;
+                    return  maiorNumero(new int[]{2, 3, 5, 1}, 0);
                 }
-
-                return  maiorNumero(new int[]{2, 3, 5, 6}, 0);
             }
-
+            return  nums[i];
         }
     }
 
