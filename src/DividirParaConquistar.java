5f894827527e7ae702808eda3ed4f6fd0bf37c38
Hiago 7k <hiago7k@gmail.com>
2026-09-21 20:47:55 -0300

    .,

--- src/DividirParaConquistar.java
+++ src/DividirParaConquistar.java
@@ -3,7 +3,7 @@ public class DividirParaConquistar {
         // RecursividadeFatorial chama = new RecursividadeFatorial();
 
         MaiorNumeroDeUmArray maior = new MaiorNumeroDeUmArray();
-        System.out.println(maior.maiorNumero(new int[]{1000, 122, 50, 10}));
+        System.out.println(maior.maiorNumero(new int[]{102, 5, 60, 1}));
     }
 
 }
--- src/MaiorNumeroDeUmArray.java
+++ src/MaiorNumeroDeUmArray.java
@@ -3,22 +3,26 @@ import java.util.Arrays;
 
 public class MaiorNumeroDeUmArray {
     private int j = 0;
-    private int target = 0;
     private int i = 0;
 
     int maiorNumero(int[] nums) {
         j++;
         i++;
+        /*
+            o Problema e que agora, ele não esta validando
+            o primeiro e o ultimo indice do array
+            por que provavelmente não esta salvando o target
+        */
 
         if (j <= nums.length -1) {
+
             if (nums[i] >= nums[i + 1]) {
-                target = nums[i];
+                return nums[i];
             } else {
-                return maiorNumero(new int[]{1000, 122, 50, 10});
+                return maiorNumero(new int[]{102, 5, 60, 1});
             }
         }
-
-        return target;
+        return  0;
     }
 }
 
