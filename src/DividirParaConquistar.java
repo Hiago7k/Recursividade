dbde7980a212d9c07e318b7c9e95c00e5d7f00f2
Hiago 7k <hiago7k@gmail.com>
2026-09-21 20:19:02 -0300

    tentando descorbir  o case base

--- src/DividirParaConquistar.java
+++ src/DividirParaConquistar.java
@@ -3,7 +3,7 @@ public class DividirParaConquistar {
         // RecursividadeFatorial chama = new RecursividadeFatorial();
 
         MaiorNumeroDeUmArray maior = new MaiorNumeroDeUmArray();
-        System.out.println(maior.maiorNumero(new int[]{2, 3, 5, 1}, 0));
+        System.out.println(maior.maiorNumero(new int[]{4, 3, 5, 1}, 0));
     }
 
 }
--- src/MaiorNumeroDeUmArray.java
+++ src/MaiorNumeroDeUmArray.java
@@ -1,20 +1,25 @@
 import java.util.Arrays;
 
+
 public class MaiorNumeroDeUmArray {
-        int maiorNumero (int[] nums, int i){
-            i = 0;
-            int j = 0;
-            int higher = 0;
-            if(j <= nums.length -1){
-                j++;
-                if (nums[i] <= nums[i+1]){
-                    higher = nums[i]; // caso base
+    private int j = 0;
+    private  int target = 0;
+
+    int maiorNumero (int[] nums, int i){
+            j++;
+            if (j < 4){
+                return  target;
+            }else {
+                target = nums[i];
+                i++;
+                if (nums[i] > nums[i+1]){
+                    target = nums[i];
                 }else {
                     i++;
-                    return  maiorNumero(new int[]{2, 3, 5, 1}, 0);
+                    return  maiorNumero(new int[]{4, 3, 5, 1}, 0);
                 }
+                return  target;
             }
-            return  nums[i];
         }
     }
 
