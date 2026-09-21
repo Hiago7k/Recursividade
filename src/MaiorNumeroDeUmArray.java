692e3a25eabc62f07a9b06b66f5ec007bfeb3f96
Hiago 7k <hiago7k@gmail.com>
2026-09-21 20:34:12 -0300

    conseguindo achar o maior numero do ARRAY utilizando a Recurssividade

--- src/MaiorNumeroDeUmArray.java
+++ src/MaiorNumeroDeUmArray.java
@@ -3,23 +3,22 @@ import java.util.Arrays;
 
 public class MaiorNumeroDeUmArray {
     private int j = 0;
-    private  int target = 0;
+    private int target = 0;
+    private int i = 0;
 
-    int maiorNumero (int[] nums, int i){
-            j++;
-            if (j < 4){
-                return  target;
-            }else {
+    int maiorNumero(int[] nums) {
+        j++;
+        i++;
+
+        if (j <= nums.length - 1) {
+
+            if (nums[i] >= nums[i + 1]) {
                 target = nums[i];
-                i++;
-                if (nums[i] > nums[i+1]){
-                    target = nums[i];
-                }else {
-                    i++;
-                    return  maiorNumero(new int[]{4, 3, 5, 1}, 0);
-                }
-                return  target;
+            } else {
+                return maiorNumero(new int[]{4, 3, 7, 1});
             }
         }
+        return target;
     }
+}
 
