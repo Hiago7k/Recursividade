0cfa7126a8ab78b5c0d3abde77d5431f4872121a
Hiago 7k <hiago7k@gmail.com>
2026-09-23 18:25:37 -0300

    Bug, estamos comparando 2 indicies por vez porem não estamos acumulando o maior valor antigo

--- src/MaiorNumeroDeUmArray.java
+++ src/MaiorNumeroDeUmArray.java
@@ -4,25 +4,24 @@ import java.util.Arrays;
 public class MaiorNumeroDeUmArray {
     private int j = 0;
     private int i = 0;
+    private int higher = 0;
 
     int maiorNumero(int[] nums) {
         j++;
         i++;
-        /*
-            o Problema e que agora, ele não esta validando
-            o primeiro e o ultimo indice do array
-            por que provavelmente não esta salvando o target
-        */
 
-        if (j <= nums.length -1) {
+        if (j >= nums.length -1) {
+            higher = higher;
+            return  higher; // caso base parada
+        }else {
 
-            if (nums[i] >= nums[i + 1]) {
-                return nums[i];
-            } else {
-                return maiorNumero(new int[]{102, 5, 60, 1});
+            if(nums[i] >= nums[i +1]){
+                higher = nums[i];
+            }else {
+                higher = nums[i+1];
             }
+            return maiorNumero(new int[]{3, 76, 20, 3});
         }
-        return  0;
     }
 }
 
