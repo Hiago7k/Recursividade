b9e91de568789683471229b7f9a7b28ccc0067b2
Hiago 7k <hiago7k@gmail.com>
2026-09-23 19:12:54 -0300

    Encontrnado o maior numero de um array

--- src/DividirParaConquistar.java
+++ src/DividirParaConquistar.java
@@ -2,8 +2,30 @@ public class DividirParaConquistar {
     static void main() {
         // RecursividadeFatorial chama = new RecursividadeFatorial();
 
-        MaiorNumeroDeUmArray maior = new MaiorNumeroDeUmArray();
-        System.out.println(maior.maiorNumero(new int[]{102, 5, 60, 1}));
+       // MaiorNumeroDeUmArray maior = new MaiorNumeroDeUmArray();
+      //  System.out.println(maior.maiorNumero(new int[]{5, 7, 2, 8, 3, 1}));
+
+        int[] nums = {5, 7, 2, 8, 3, 1};
+        int sentinela = nums[0];
+        int higher = 0;
+
+        for (int i = 0; i <= nums.length -1; i++){
+            for (int j = i + 1; j <= nums.length -1; j++){
+
+                if (nums[i] >= nums[j]){
+                    higher = nums[i];
+                }else {
+                    higher = nums[j];
+                }
+
+                if (higher >= sentinela){
+                    sentinela = higher;
+                }else {
+                    sentinela = sentinela;
+                }
+            }
+        }
+        System.out.println(sentinela);
     }
 
 }
